cf629d6545bd76827dab5d74465b344051dca5e8
utkarshhhhh <s.utkarsh2001@gmail.com>
2022-03-17 20:24:44 +0530

    SortPartialCompleted

---
+++ level1/Dynamic Programming/TimeAndSpaceComplexity/mergeTwoSortedArr.java
@@ -0,0 +1,51 @@
+import java.util.*;
+
+public class mergeTwoSortedArr {
+
+  public static int[] mergeTwoSortedArrays(int[] a, int[] b){
+    //write your code here
+    int n = a.length, m = b.length;
+    int[] c = new int[n+m];
+    int i=0 , j = 0, k=0;
+    while ( i<n && j<m ) {
+        if( a[i] < b[j] ){
+            c[k++] = a[i++];
+        }else{
+            c[k++] = b[j++];
+        }
+    }
+
+    while (i<n) {
+        c[k++] = a[i++];
+    }
+    
+    while (j<m) {
+        c[k++] = b[j++];
+    }
+
+    return c;
+  }
+
+  public static void print(int[] arr){
+    for(int i = 0 ; i < arr.length; i++){
+      System.out.println(arr[i]);
+    }
+  }
+  public static void main(String[] args){
+    Scanner scn = new Scanner(System.in);
+    int n = scn.nextInt();
+    int[] a = new int[n];
+    for(int i = 0 ; i < n; i++){
+      a[i] = scn.nextInt();
+    }
+    int m = scn.nextInt();
+    int[] b = new int[m];
+    for(int i = 0 ; i < m; i++){
+      b[i] = scn.nextInt();
+    }
+    int[] mergedArray = mergeTwoSortedArrays(a,b);
+    print(mergedArray);
+    scn.close();
+  }
+
+}
